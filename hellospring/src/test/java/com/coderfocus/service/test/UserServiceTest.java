fe6b4c92d9f6d0e84a7f73a48b66b79602cf419a
coderfocus <songwenjie830@gmail.com>
2019-12-06 16:26:01 +0800

    feat(junit): 新增junit断言

--- hellospring/src/test/java/com/coderfocus/service/test/UserServiceTest.java
+++ hellospring/src/test/java/com/coderfocus/service/test/UserServiceTest.java
@@ -46,4 +46,42 @@ public class UserServiceTest {
     public void ignore(){
         System.out.println("ignore");
     }
+
+    @Test
+    public void testAssert() {
+        String obj1 = "junit";
+        String obj2 = "junit";
+        String obj3 = "test";
+        String obj4 = "test";
+        String obj5 = null;
+        int var1 = 1;
+        int var2 = 2;
+        int var3 = 1;
+        int[] arithmetic1 = {1, 2, 3};
+        int[] arithmetic2 = {1, 2, 3};
+
+        //断言值相等
+        Assert.assertEquals(obj1, obj2);
+
+        //断言两个对象引用相同的对象
+        Assert.assertSame(obj3, obj4);
+
+        //断言，两个对象不是引用同一个对象
+        Assert.assertNotSame(obj2, obj4);
+
+        //断言一个对象不为空(null)
+        Assert.assertNotNull(obj1);
+
+        //断言一个对象为空(null)
+        Assert.assertNull(obj5);
+
+        //断言一个条件为假
+        Assert.assertFalse("为假", var1 == var2);
+
+        //断言一个条件为真
+        Assert.assertTrue("为真", var1 == var3);
+
+        //断言预期数组和结果数组相等
+        Assert.assertArrayEquals(arithmetic1, arithmetic2);
+    }
 }
